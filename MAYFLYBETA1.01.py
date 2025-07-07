61bb3c439a0b0a44ac73baffe70d31c4ff897a36
natclaxton <nathaniel.claxton@ba.com>
2025-07-07 14:02:10 +0100

    Update MAYFLYBETA1.01.py

--- MAYFLYBETA1.01.py
+++ MAYFLYBETA1.01.py
@@ -52,22 +52,24 @@ st.markdown(
 st.markdown("---")
 
 # === Flight Lists & Definitions ===
-DOMESTIC_ROUTES = ["LHRABZ","LHRINV","LHRGLA","LHREDI","LHRBHD","LHRNCL","LHRJER","LHRMAN","LHRBFS","LHRDUB"]
+DOMESTIC_ROUTES = ["LHRABZ","LHRINV","LHRGLA","LHREDI","LHRBHD",
+                   "LHRNCL","LHRJER","LHRMAN","LHRBFS","LHRDUB"]
 T3_FLIGHTS = [  # BA820 removed
-    "BA159","BA227","BA247","BA253","BA289","BA336","BA340","BA350","BA366","BA368","BA370",
-    "BA372","BA374","BA376","BA378","BA380","BA382","BA386","BA408","BA416","BA418",
-    "BA422","BA490","BA492","BA498","BA532","BA608","BA616","BA618","BA690","BA696","BA700",
-    "BA702","BA704","BA706","BA760","BA762","BA764","BA766","BA770","BA790","BA792","BA802",
-    "BA806","BA848","BA852","BA854","BA856","BA858","BA860","BA862","BA864","BA866","BA868",
+    "BA159","BA227","BA247","BA253","BA289","BA336","BA340","BA350","BA366",
+    "BA368","BA370","BA372","BA374","BA376","BA378","BA380","BA382","BA386",
+    "BA408","BA416","BA418","BA422","BA490","BA492","BA498","BA532","BA608",
+    "BA616","BA618","BA690","BA696","BA700","BA702","BA704","BA706","BA760",
+    "BA762","BA764","BA766","BA770","BA790","BA792","BA802","BA806","BA848",
+    "BA852","BA854","BA856","BA858","BA860","BA862","BA864","BA866","BA868",
     "BA870","BA872","BA874","BA882","BA884","BA886","BA892","BA896","BA918","BA920"
 ]
 LGW_FLIGHTS = [
-    "BA2640","BA2704","BA2670","BA2740","BA2624","BA2748","BA2676","BA2758","BA2784","BA2610",
-    "BA2606","BA2574","BA2810","BA2666","BA2614","BA2716","BA2808","BA2660","BA2680","BA2720",
-    "BA2642","BA2520","BA2161","BA2037","BA2754","BA2239","BA1480","BA2159","BA2167","BA2780",
-    "BA2203","BA2702","BA2756","BA2263","BA2612","BA2794","BA2039","BA2812","BA2752","BA2273",
-    "BA2602","BA2682","BA2662","BA2608","BA2644","BA2650","BA2576","BA2590","BA2722","BA2816",
-    "BA2596","BA2656","BA2668","BA2672","BA2572"
+    "BA2640","BA2704","BA2670","BA2740","BA2624","BA2748","BA2676","BA2758","BA2784",
+    "BA2610","BA2606","BA2574","BA2810","BA2666","BA2614","BA2716","BA2808","BA2660",
+    "BA2680","BA2720","BA2642","BA2520","BA2161","BA2037","BA2754","BA2239","BA1480",
+    "BA2159","BA2167","BA2780","BA2203","BA2702","BA2756","BA2263","BA2612","BA2794",
+    "BA2039","BA2812","BA2752","BA2273","BA2602","BA2682","BA2662","BA2608","BA2644",
+    "BA2650","BA2576","BA2590","BA2722","BA2816","BA2596","BA2656","BA2668","BA2672","BA2572"
 ]
 SHORT_HAUL_TYPES = ["320","32N","32Q","319","32A"]
 
@@ -164,7 +166,8 @@ station = st.selectbox("", ["All Stations","T3","T5","LGW"])
 
 st.markdown("<h4 style='color:#3e577d;'>CHOOSE FILTERS</h4>", unsafe_allow_html=True)
 filter_options = st.multiselect(
-    "", options=["All Flights","Flights above 90%","Flights above 70%","Domestic","Short Haul"], default=["All Flights"]
+    "", options=["All Flights","Flights above 90%","Flights above 70%","Domestic","Short Haul"],
+    default=["All Flights"]
 )
 
 st.markdown("<h4 style='color:#3e577d;'>FILTER BY DEPARTURE HOUR</h4>", unsafe_allow_html=True)
@@ -187,6 +190,8 @@ h, r = divmod(secs, 3600)
 m, s = divmod(r, 60)
 bst_time = next_time + timedelta(hours=1)
 st.markdown(f"**Next refresh: {next_time.strftime('%H:%M')} UTC / {bst_time.strftime('%H:%M')} BST in {h:02d}:{m:02d}:{s:02d}**")
+# Added hyperlink to OpsDashboard:
+st.markdown("[OpsDashboard](https://opsdashboard.baplc.com/#/search)")
 
 if text_input:
     df = parse_txt(text_input)
